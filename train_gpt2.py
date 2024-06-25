4e36a97bf2a28e824ae6f45e9c579f9956f4d1bd
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-06-25 09:03:01 +0530

    added Block

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,13 +3,27 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.layerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.layerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self, ln_1(x))
+        x = x + self.mlp(self, ln_2(x))
+        return x
+        
 @dataclass
 class GPTConfig:
     block_size: int = 256
     vocab_size: int = 65
     n_layer   : int = 6
     n_head    : int = 6
-    n_embd   : int = 384
+    n_embd    : int = 384
 
 class GPT(nn.Module):
 
